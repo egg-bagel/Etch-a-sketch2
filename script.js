84cfc7345b32de50a58397ba7f557302c9b5b5c8
Quinn Hertel <98055166+egg-bagel@users.noreply.github.com>
2023-01-31 19:28:24 -0500

    Added function to color squares when the user mouses over

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const container = document.querySelector("#container");
 
+//Creates a 16x16 grid
+
 function createGrid() {
     for (let i=0; i<16; i++) {
         let row = document.createElement("div");
-        row.classList.add("row");
         container.appendChild(row);
+        row.classList.add("row");
         for (let j=0; j<16; j++) {
             let cell = document.createElement("div");
-            cell.classList.add("cell");
             row.appendChild(cell);
+            cell.classList.add("cell");
         }
     }
 }
+
+//Colors in boxes on the grid on mouseover
+
+function draw() {
+    let cells = container.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "black";
+        })
+    })
+}
+
+createGrid();
+draw();
