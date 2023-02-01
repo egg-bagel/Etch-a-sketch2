68fc3bbcb5c1cda4d8a47bd05ec4383d0bd5fc7f
Quinn Hertel <98055166+egg-bagel@users.noreply.github.com>
2023-01-31 20:47:00 -0500

    Added random colors and gradual darkening of colors

--- script.js
+++ script.js
@@ -16,13 +16,45 @@ function createGrid(numberOfSquares) {
     }
 }
 
+//Generates a random RGB color.
+
+function getRandomColor() {
+    let r = (Math.floor(Math.random() * 255));
+    let g = (Math.floor(Math.random() * 255));
+    let b = (Math.floor(Math.random() * 255));
+    let result = "rgb(" + r + ", " + g + ", " + b + ")";
+    return result;
+}
+
+//Makes each mouse-pass over a square darken the color by ten percent.
+
+function darkenColor(currentColor) {
+    let numberString = currentColor.substr(4).slice(0, -1);
+    let numberArray = numberString.split(", ");
+    let x = parseInt(numberArray[0]);
+    let y = parseInt(numberArray[1]);
+    let z = parseInt(numberArray[2]);
+    x = Math.floor(x - (0.1 * x));
+    y = Math.floor(y - (0.1 * y));
+    z = Math.floor(z - (0.1 * z));
+    return `rgb(${x}, ${y}, ${z})`;
+}
+
 //Colors in boxes on the grid on mouseover
 
 function draw() {
     let cells = container.querySelectorAll(".cell");
     cells.forEach(cell => {
         cell.addEventListener("mouseover", () => {
-            cell.style.backgroundColor = "black";
+            if (cell.classList.contains("marked")) {
+                let currentColor = cell.style.backgroundColor;
+                let newColor = darkenColor(currentColor);
+                cell.style.backgroundColor = newColor;
+            }
+            else if (cell.classList.contains("marked") === false) {
+                cell.style.backgroundColor = getRandomColor();
+                cell.classList.add("marked");
+            }
         })
     })
 }
